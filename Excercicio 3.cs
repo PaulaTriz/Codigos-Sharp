245708b9cca580d49add8db69239c9f26e170c70
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 09:37:38 -0300

    Add files via upload

---
+++ Excercicio 3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um ponto flutuante");
+        float floatint = int.Parse(Console.ReadLine());
+        Console.WriteLine("Número digitado " + floatint);
+    }
+}

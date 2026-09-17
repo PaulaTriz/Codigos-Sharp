08e66dc97bb166ee28ffe325333e4682e661e0e4
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 11:17:19 -0300

    Add files via upload

---
+++ Excercicio 7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o nome do usuario: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a idade do usuario: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nome " + nome + " | idade " + idade + " anos ");
+
+
+    }
+}

1ba110adaf98fdaefe0bcf60f86e9116642be41d
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 16:44:58 -0300

    Add files via upload

---
+++ Excercicio 8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Progama
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o preço do produto: ");
+        double preço = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o desconto do produto: ");
+        double desconto = double.Parse(Console.ReadLine());
+
+        double precoFinal = preço - (preço * (desconto / 100));
+        Console.Write("Preço final com desconto: R$ " + precoFinal);
+    }
+}

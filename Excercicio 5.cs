44550a4cb683a0f27674210e8e59300b9ffb10c2
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 10:45:01 -0300

    Add files via upload

---
+++ Excercicio 5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write(" Digite um caracter: ");
+        char caracter = char.Parse(Console.ReadLine());
+        Console.WriteLine("Caracter Resultado: " + caracter);
+    }
+}
+

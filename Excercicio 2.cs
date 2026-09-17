dda8b8869921b8cc45c56e3420b68024cf2b00d2
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 09:28:50 -0300

    Add files via upload

---
+++ Excercicio 2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número real ");
+        double doubleint = int.Parse(Console.ReadLine());
+        Console.WriteLine("Número digitado: " + doubleint);
+    }
+
+}

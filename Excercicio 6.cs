64a845de4a7e97c9bcff2d3b3641b2681f27c57b
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 10:59:19 -0300

    Add files via upload

---
+++ Excercicio 6.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número decimal: ");
+        decimal Numerodecimal = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Número digitado: " + Numerodecimal);
+    }
+}

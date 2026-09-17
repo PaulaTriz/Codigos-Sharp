f6f406a1ad55823562dd5c6013e5948ea3fb53a7
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 09:21:13 -0300

    Add files via upload

---
+++ Excercicio 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número inteiro");
+        int numeroInt = int.Parse(Console.ReadLine());
+        Console.WriteLine("Número digitado: " + numeroInt);
+    }
+
+}
+
+
+
+
+

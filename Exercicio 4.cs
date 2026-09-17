d9ee81edc197d30554b3cede89c7593e61461f6a
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-17 10:34:52 -0300

    Add files via upload

---
+++ Exercicio 4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite 'sim' ou 'não' ");
+        string entrada = Console.ReadLine().ToLower().Trim();
+        bool respostaBool = (entrada == "sim");
+        Console.WriteLine(" Valor boleano " + respostaBool);
+        {
+
+        }
+
+    }
+}
